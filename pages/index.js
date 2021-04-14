db19d68c2b15147c35a334c38d839a9c7f097307
Matthew Salvador <matthew.jsalvador@gmail.com>
2021-04-14 00:21:59 -0400

    navbar and header styling complete

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,41 @@ export default function Home() {
           <nav className={styles.navigation}>
             <a className={styles.links}>Docs</a>
             <a className={styles.links}>Tutorial</a>
-            <a className={styles.links}> Blog</a>
+            <a className={styles.links}>Blog</a>
             <a className={styles.links}>Github</a>
           </nav>
         </div>
       </div>
-      <div className={styles.bodyContainer}>world</div>
-      <div className={styles.footerContainer}>test</div>
+      <div className={styles.bodyContainer}>
+        <div className={styles.body}>
+          <header className={styles.banner}>
+            <div className={styles.bannerTextContainer}>
+              <h1 className={styles.bannerH1Text}>next-persist</h1>
+              <p className={styles.bannerPText}>Bridging the gap between client-side persistence and server-side rendering.</p>
+              <button className={styles.bannerButton}>Get Started</button>
+            </div>
+          </header>
+          <div className={styles.content}>
+            <div className={styles.principles}>
+              <div className={styles.blurb}>
+
+              </div>
+              <div className={styles.blurb}>
+
+              </div>
+              <div className={styles.blurb}>
+
+              </div>
+            </div>
+            <div className={styles.implementation}>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footerContainer}>
+
+      </div>
       {/* <Head className={styles.headContainer}>
         <title>next-persist</title>
         <link rel="icon" href="/favicon.ico" />
