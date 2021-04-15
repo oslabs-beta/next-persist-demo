348ea168b146ca0932b3627b9e279ee9079df72b
Christopher Bosserman <christopherpbosserman@gmail.com>
2021-04-15 08:14:38 -0400

    fixed title on demo

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import Link from 'next/link';
-import Head from 'next/head';
-import Header from '../components/LandingComponents/Header.jsx'
-import Body from '../components/LandingComponents/Body.jsx'
-import Footer from '../components/LandingComponents/Footer.jsx'
+
+import Header from '../components/LandingComponents/Header.jsx';
+import Body from '../components/LandingComponents/Body.jsx';
+import Footer from '../components/LandingComponents/Footer.jsx';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>next-persist - A node module by most.js</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
       <Body />
       <Footer />
